74174d87ad86323f9e7d120c5528515a117bcc1e
Chapman Ou <ochapman@ochapman.cn>
2014-06-12 12:44:11 +0800

    Add OEM strings

--- godmi.go
+++ godmi.go
@@ -2018,6 +2018,27 @@ func (s SystemSlot) String() string {
 	return fmt.Sprintf("System Slot: %s\n\t\tSlot Designation: %s\n\t\tSlot Type: %s\n\t\tSlot Data Bus Width: %s\n\t\tCurrent Usage: %s\n\t\tSlot Length: %s\n\t\tSlot ID: %s\n\t\tSlot Characteristics1: %s\n\t\tSlot Characteristics2: %s\n\t\tSegment Group Number: %s\n\t\tBus Number: %s\n\t\tDevice/Function Number: %s\n", s.Designation, s.Type, s.DataBusWidth, s.CurrentUsage, s.Length, s.ID, s.Characteristics1, s.Characteristics2, s.SegmentGroupNumber, s.BusNumber, s.DeviceFunctionNumber)
 }
 
+//Type 11
+type OEMStrings struct {
+	InfoCommon
+	Count   byte
+	strings string
+}
+
+func (h DMIHeader) OEMStrings() OEMStrings {
+	var o OEMStrings
+	data := h.data
+	o.Count = data[0x04]
+	for i := byte(0); i < o.Count; i++ {
+		o.strings += fmt.Sprintf("strings: %d %s\n\t\t", i, h.FieldString(int(data[i])))
+	}
+	return o
+}
+
+func (o OEMStrings) String() string {
+	return fmt.Sprintf("OEM strings: %s", o.strings)
+}
+
 func U16(data []byte) uint16 {
 	var u16 uint16
 	binary.Read(bytes.NewBuffer(data[0:2]), binary.LittleEndian, &u16)
