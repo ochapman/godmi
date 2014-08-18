2c8131418bbced303f7b0abff451d008580cc429
Chapman Ou <ochapman@ochapman.cn>
2014-08-18 23:02:12 +0800

    Move Baseboard to type2_baseboard.go

--- godmi.go
+++ godmi.go
@@ -150,100 +150,6 @@ type dmiHeader struct {
 	data []byte
 }
 
-type FeatureFlags byte
-
-func (f FeatureFlags) String() string {
-	features := [...]string{
-		"Board is a hosting board", /* 0 */
-		"Board requires at least one daughter board",
-		"Board is removable",
-		"Board is replaceable",
-		"Board is hot swappable", /* 4 */
-	}
-	var s string
-	for i := uint32(0); i < 5; i++ {
-		if f&(1<<i) != 0 {
-			s += "\n\t\t" + features[i]
-		}
-	}
-	return s
-}
-
-type BoardType byte
-
-const (
-	BoardTypeUnknown BoardType = 1 + iota
-	BoardTypeOther
-	BoardTypeServerBlade
-	BoardTypeConnectivitySwitch
-	BoardTypeSystemManagementModule
-	BoardTypeProcessorModule
-	BoardTypeIOModule
-	BoardTypeMemModule
-	BoardTypeDaughterBoard
-	BoardTypeMotherboard
-	BoardTypeProcessorMemmoryModule
-	BoardTypeProcessorIOModule
-	BoardTypeInterconnectBoard
-)
-
-func (b BoardType) String() string {
-	types := [...]string{
-		"Unknown", /* 0x01 */
-		"Other",
-		"Server Blade",
-		"Connectivity Switch",
-		"System Management Module",
-		"Processor Module",
-		"I/O Module",
-		"Memory Module",
-		"Daughter Board",
-		"Motherboard",
-		"Processor+Memory Module",
-		"Processor+I/O Module",
-		"Interconnect Board", /* 0x0D */
-	}
-	if b > BoardTypeUnknown && b < BoardTypeInterconnectBoard {
-		return types[b-1]
-	}
-	return "Out Of Spec"
-}
-
-type BaseboardInformation struct {
-	infoCommon
-	Manufacturer                   string
-	ProductName                    string
-	Version                        string
-	SerialNumber                   string
-	AssetTag                       string
-	FeatureFlags                   FeatureFlags
-	LocationInChassis              string
-	ChassisHandle                  uint16
-	BoardType                      BoardType
-	NumberOfContainedObjectHandles byte
-	ContainedObjectHandles         []byte
-}
-
-func (b BaseboardInformation) String() string {
-	return fmt.Sprintf("Base Board Information\n"+
-		"\tManufacturer: %s\n"+
-		"\tProduct Name: %s\n"+
-		"\tVersion: %s\n"+
-		"\tSerial Number: %s\n"+
-		"\tAsset Tag: %s\n"+
-		"\tFeatures:%s\n"+
-		"\tLocation In Chassis: %s\n"+
-		"\tType: %s",
-		b.Manufacturer,
-		b.ProductName,
-		b.Version,
-		b.SerialNumber,
-		b.AssetTag,
-		b.FeatureFlags,
-		b.LocationInChassis,
-		b.BoardType)
-}
-
 // BIOS Characteristics
 const (
 	BIOSCharacteristicsReserved0 BIOSCharacteristics = 1 << iota
@@ -304,16 +210,6 @@ const (
 	BIOSCharacteristicsExt2UEFISpecSupported
 	BIOSCharacteristicsExt2VirtualMachine
 	// Bits 5:7 Reserved for future assignment
-)
-
-// Baseboard feature flags
-const (
-	FeatureFlagsHostingBoard = 1 << iota
-	FeatureFlagsAtLeastOneDaughter
-	FeatureFlagsRemovable
-	FeatureFlagsRepleaceable
-	FeatureFlagsHotSwappable
-	//FeatureFlagsReserved = 000b
 )
 
 type ChassisType byte
