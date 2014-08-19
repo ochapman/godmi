c9f765e94e6f7edcd782fc81d23815936fbda75e
Chapman Ou <ochapman@ochapman.cn>
2014-08-20 07:58:28 +0800

    Move 64BitMemoryError from godmi.go to type33_64bit_memory_error.go

--- godmi.go
+++ godmi.go
@@ -564,35 +564,6 @@ func (h dmiHeader) SystemBootInformation() *SystemBootInformation {
 	}
 }
 
-type _64BitMemoryErrorInformation struct {
-	infoCommon
-	Type              MemoryErrorInformationType
-	Granularity       MemoryErrorInformationGranularity
-	Operation         MemoryErrorInformationOperation
-	VendorSyndrome    uint32
-	ArrayErrorAddress uint32
-	ErrorAddress      uint32
-	Reslution         uint32
-}
-
-func (m _64BitMemoryErrorInformation) String() string {
-	return fmt.Sprintf("_64 Bit Memory Error Information\n"+
-		"\tType: %s\n"+
-		"\tGranularity: %s\n"+
-		"\tOperation: %s\n"+
-		"\tVendor Syndrome: %d\n"+
-		"\tArray Error Address: %d\n"+
-		"\tError Address: %d\n"+
-		"\tReslution: %d",
-		m.Type,
-		m.Granularity,
-		m.Operation,
-		m.VendorSyndrome,
-		m.ArrayErrorAddress,
-		m.ErrorAddress,
-		m.Reslution)
-}
-
 func (h dmiHeader) _64BitMemoryErrorInformation() *_64BitMemoryErrorInformation {
 	data := h.data
 	return &_64BitMemoryErrorInformation{
