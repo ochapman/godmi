b3fb7e6e006fd738ceca45590660e727cc8b1cad
Chapman Ou <ochapman@ochapman.cn>
2014-08-19 20:10:46 +0800

    Move HardwareSecurity from godmi.go to type24_hardware_security.go

--- godmi.go
+++ godmi.go
@@ -469,68 +469,11 @@ func (h dmiHeader) SystemReset() *SystemReset {
 	}
 }
 
-type HardwareSecurityStatus byte
-
-const (
-	HardwareSecurityStatusDisabled HardwareSecurityStatus = iota
-	HardwareSecurityStatusEnabled
-	HardwareSecurityStatusNotImplemented
-	HardwareSecurityStatusUnknown
-)
-
-func (h HardwareSecurityStatus) String() string {
-	status := [...]string{
-		"Disabled",
-		"Enabled",
-		"Not Implemented",
-		"Unknown",
-	}
-	return status[h]
-}
-
-type HardwareSecuritySettings struct {
-	PowerOnPassword       HardwareSecurityStatus
-	KeyboardPassword      HardwareSecurityStatus
-	AdministratorPassword HardwareSecurityStatus
-	FrontPanelReset       HardwareSecurityStatus
-}
-
-func NewHardwareSecurity(data byte) HardwareSecuritySettings {
-	var h HardwareSecuritySettings
-	h.PowerOnPassword = HardwareSecurityStatus(data & 0xC0)
-	h.KeyboardPassword = HardwareSecurityStatus(data & 0x30)
-	h.AdministratorPassword = HardwareSecurityStatus(data & 0x0C)
-	h.FrontPanelReset = HardwareSecurityStatus(data & 0x03)
-	return h
-}
-
-func (h HardwareSecuritySettings) String() string {
-	return fmt.Sprintf("Power-on Password Status: %s\n"+
-		"Keyboard Password Status: %s\n"+
-		"Administrator Password Status: %s\n"+
-		"Front Panel Reset Status: %s\n",
-		h.PowerOnPassword,
-		h.KeyboardPassword,
-		h.AdministratorPassword,
-		h.FrontPanelReset)
-}
-
-type HardwareSecurity struct {
-	infoCommon
-	Setting HardwareSecuritySettings
-}
-
 func (h dmiHeader) HardwareSecurity() *HardwareSecurity {
 	var hw HardwareSecurity
 	data := h.data
 	hw.Setting = NewHardwareSecurity(data[0x04])
 	return &hw
-}
-
-func (h HardwareSecurity) String() string {
-	return fmt.Sprintf("Hardware Security\n"+
-		"\tSetting: %s\n",
-		h.Setting)
 }
 
 type SystemPowerControlsMonth byte
