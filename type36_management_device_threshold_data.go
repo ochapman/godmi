da7e467d0173e2899c2f6b99ab5bc890eab3c328
Chapman Ou <ochapman@ochapman.cn>
2014-08-21 07:38:15 +0800

    Move ManagementDeviceThresholdData from godmi.go to type36_management_device_threshold_data.go

--- type36_management_device_threshold_data.go
+++ type36_management_device_threshold_data.go
@@ -6,4 +6,32 @@
 */
 package godmi
 
-//TODO:
+import (
+	"fmt"
+)
+
+type ManagementDeviceThresholdData struct {
+	infoCommon
+	LowerThresholdNonCritical    uint16
+	UpperThresholdNonCritical    uint16
+	LowerThresholdCritical       uint16
+	UpperThresholdCritical       uint16
+	LowerThresholdNonRecoverable uint16
+	UpperThresholdNonRecoverable uint16
+}
+
+func (m ManagementDeviceThresholdData) String() string {
+	return fmt.Sprintf("Management Device Threshold Data\n"+
+		"\tLower Threshold Non Critical: %d\n"+
+		"\tUpper Threshold Non Critical: %d\n"+
+		"\tLower Threshold Critical: %d\n"+
+		"\tUpper Threshold Critical: %d\n"+
+		"\tLower Threshold Non Recoverable: %d\n"+
+		"\tUpper Threshold Non Recoverable: %d",
+		m.LowerThresholdNonCritical,
+		m.UpperThresholdNonCritical,
+		m.LowerThresholdCritical,
+		m.UpperThresholdCritical,
+		m.LowerThresholdNonRecoverable,
+		m.UpperThresholdNonRecoverable)
+}
