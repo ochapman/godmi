73bdd5122744731a01fa06842bd1120dadbdc20b
Chapman Ou <ochapman@ochapman.cn>
2014-08-20 20:29:47 +0800

    Move ManagementDeviceComponent from godmi.go to type35_management_device_componnent.go

--- godmi.go
+++ godmi.go
@@ -587,26 +587,6 @@ func (h dmiHeader) ManagementDevice() *ManagementDevice {
 	}
 }
 
-type ManagementDeviceComponent struct {
-	infoCommon
-	Description            string
-	ManagementDeviceHandle uint16
-	ComponentHandle        uint16
-	ThresholdHandle        uint16
-}
-
-func (m ManagementDeviceComponent) String() string {
-	return fmt.Sprintf("Management Device Component\n"+
-		"\tDescription: %s\n"+
-		"\tManagement Device Handle: %d\n"+
-		"\tComponent Handle: %d\n"+
-		"\tThreshold Handle: %d",
-		m.Description,
-		m.ManagementDeviceHandle,
-		m.ComponentHandle,
-		m.ThresholdHandle)
-}
-
 func (h dmiHeader) ManagementDeviceComponent() *ManagementDeviceComponent {
 	data := h.data
 	return &ManagementDeviceComponent{
